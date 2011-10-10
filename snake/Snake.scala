1abc9f813dee93238a34cd643724c58f24a3638f
Sylvain MATHIEU <sylvain.mathieu@gmail.com>
2011-10-10 23:57:57 +0100

    Grid handling

--- snake/Snake.scala
+++ snake/Snake.scala
@@ -4,7 +4,7 @@ import swing.{SimpleSwingApplication, MainFrame, Panel}
 
 object App extends SimpleSwingApplication {
 
-	import java.awt.{Dimension, Graphics2D}
+	import java.awt.{Dimension, Graphics2D, Rectangle}
 	import java.awt.{Color}
 	import java.awt.event.{ActionEvent}
 	import javax.swing.{Timer, AbstractAction}
@@ -17,13 +17,13 @@ object App extends SimpleSwingApplication {
 		title = "Scala Snake"
 		contents = mainPanel
 
-		lazy val mainPanel = new Panel() {
+		lazy val mainPanel = new Panel {
 			focusable = true
 			background = Color.black
 			preferredSize = new Dimension(200, 200)
 
 			override def paint(g: Graphics2D) {
-				g.setColor(Color.black)
+				g.setColor(new Color(0x1B, 0x00, 0x3F))
 				g.fillRect(0, 0, size.width, size.height)
 				onPaint(g)
 			}
@@ -32,6 +32,7 @@ object App extends SimpleSwingApplication {
 		val timer = new Timer(100, new AbstractAction() {
 			override def actionPerformed(e: ActionEvent) {
 				game = game.tick
+				repaint()
 			}
 		})
 
@@ -41,20 +42,53 @@ object App extends SimpleSwingApplication {
 
 	def onPaint(g: Graphics2D) {
 
+		def drawBlock(x: Int, y: Int) {
+			g.draw(new Rectangle(x * 10, y * 10, 10, 10))
+		}
+
+		g.setColor(Color.gray)
+
+		game.grid.set(5, 10, 1)
+
+		// Draw grid
+		game.grid.forEach { (x: Int, y: Int) =>
+			if (game.grid.get(x, y) > 0) drawBlock(x, y)
+		}
 	}
 
 }
 
 object Game {
 
-	def newGame = new Game
+	def newGame = {
+		new Game(new Grid(20, 20))
+	}
 
 }
 
-class Game {
+class Game(val grid: Grid) {
 
 	def tick: Game = {
 		this
 	}
 
+	def move = {
+
+	}
+
+}
+
+class Grid(width: Int, height: Int) {
+
+	var grid = (for (y <- 0 until height) yield (for (x <- 0 until width) yield 0).toArray).toArray
+
+	def forEach(func: (Int, Int) => Any) {
+		for (y <- 0 until height)
+			for (x <- 0 until width)
+				func(x, y)
+	}
+
+	def set(x: Int, y: Int, value: Int) = grid(x)(y) = value
+
+	def get(x: Int, y: Int): Int = grid(x)(y)
 }
